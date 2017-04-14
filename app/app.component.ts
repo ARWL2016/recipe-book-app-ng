5fe86633538df6e66f8fce7ef3b93d2c76760a06
Alistair Willis <alistairrwillis@gmail.com>
2017-04-14 13:18:57 +0900

    initial commit

---
+++ app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'rb-app',
+    template: `
+    <h1>Angular2: Getting Started</h1>
+    <index></index>
+    `
+    
+})
+export class AppComponent { }
---
+++ app/recipe/recipe-index.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'index',
+  templateUrl: 'app/recipe/recipe-index.html'
+})
+export class RecipeIndex {
+
+}
